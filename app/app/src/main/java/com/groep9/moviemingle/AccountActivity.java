da2a9c81762d77573da9357d0f9902fa6113b33b
RenzeWest <rg.westerink@student.avans.nl>
2024-03-27 15:39:08 +0100

    [feat] basis van de XML voor het account scherm aangemaakt. (#4)

---
+++ app/app/src/main/java/com/groep9/moviemingle/AccountActivity.java
@@ -0,0 +1,14 @@
+package com.groep9.moviemingle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account);
+    }
+}
--- app/app/src/main/java/com/groep9/moviemingle/MainActivity.java
+++ app/app/src/main/java/com/groep9/moviemingle/MainActivity.java
@@ -2,7 +2,9 @@ package com.groep9.moviemingle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button accountButton = findViewById(R.id.btn_main_account);
+
+        accountButton.setOnClickListener(view -> {
+            Intent accountIntent = new Intent(this, AccountActivity.class);
+            startActivity(accountIntent);
+        });
+
     }
 }
